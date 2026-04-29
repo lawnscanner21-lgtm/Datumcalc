5fb0700ead6fa60fdaab9c19bd5a2960c130176b
alsheikh <lawnscanner21@gmail.com>
2026-04-30 00:44:59 +0500

    2222

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,13 +2,7 @@ import createMiddleware from 'next-intl/middleware';
 import { routing } from './i18n/routing';
 import { NextRequest } from 'next/server';
 
-const intlMiddleware = createMiddleware(routing);
-
-export const proxy = (request: NextRequest) => {
-    return intlMiddleware(request);
-};
-
-export default proxy;
+export default createMiddleware(routing);
 
 export const config = {
     // Match only internationalized pathnames
